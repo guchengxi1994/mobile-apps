21a732fcb0f69e1a5ca207431ddcbcca87bf9700
guchengxi1994 <guchengxi1994@users.noreply.github.com>
2021-06-18 16:35:00 +0800

    update

---
+++ password_store_app/lib/main/view/main_list_page.dart
@@ -0,0 +1,74 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: xiaoshuyui
+ * @email: guchengxi1994@qq.com
+ * @Date: 2021-06-16 19:03:37
+ * @LastEditors: xiaoshuyui
+ * @LastEditTime: 2021-06-18 16:18:24
+ */
+
+part of 'package:password_store_app/main/view/main_page.dart';
+
+class UserDataList extends StatefulWidget {
+  @override
+  _UserDataListState createState() => _UserDataListState();
+}
+
+class _UserDataListState extends State<UserDataList> {
+  final _scrollController = ScrollController();
+  late MainBloc _mainBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _mainBloc = context.read<MainBloc>();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MainBloc, MainState>(builder: (context, state) {
+      // print(state.userDatas.length);
+      return Scaffold(
+        body: ListView.builder(
+            controller: _scrollController,
+            itemCount: state.userDatas.length,
+            itemBuilder: (BuildContext context, int index) {
+              if (index < state.userDatas.length) {
+                return UserDataWidget(index);
+              } else {
+                return Container(
+                  child: Text("无更多"),
+                );
+              }
+            }),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            size: 40,
+            color: Colors.black,
+          ),
+          onPressed: () async {
+            // print('FloatingActionButton');
+            UserData result = await Navigator.of(context)
+                .pushNamed(Routers.createUserData) as UserData;
+
+            /// fake result
+            if (result != null) {
+              // result.appname = "cccc";
+              // result.userId = "asdasd";
+              _mainBloc.add(DataAdded(userData: result));
+            }
+          },
+          backgroundColor: Colors.yellow,
+        ),
+      );
+    });
+  }
+}
--- password_store_app/lib/main/view/main_page.dart
+++ password_store_app/lib/main/view/main_page.dart
@@ -1,3 +1,13 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: xiaoshuyui
+ * @email: guchengxi1994@qq.com
+ * @Date: 2021-06-18 16:17:05
+ * @LastEditors: xiaoshuyui
+ * @LastEditTime: 2021-06-18 16:19:21
+ */
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:password_store_app/entity/userdata.dart';
@@ -6,68 +16,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:password_store_app/main/widget/list_tile_component_bloc.dart';
 import 'package:password_store_app/utils/routers.dart';
 
-class UserDataList extends StatefulWidget {
-  @override
-  _UserDataListState createState() => _UserDataListState();
-}
-
-class _UserDataListState extends State<UserDataList> {
-  final _scrollController = ScrollController();
-  late MainBloc _mainBloc;
-
-  @override
-  void initState() {
-    super.initState();
-    _mainBloc = context.read<MainBloc>();
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<MainBloc, MainState>(builder: (context, state) {
-      // print(state.userDatas.length);
-      return Scaffold(
-        body: ListView.builder(
-            controller: _scrollController,
-            itemCount: state.userDatas.length,
-            itemBuilder: (BuildContext context, int index) {
-              if (index < state.userDatas.length) {
-                return UserDataWidget(index);
-              } else {
-                return Container(
-                  child: Text("无更多"),
-                );
-              }
-            }),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(
-            Icons.add,
-            size: 40,
-            color: Colors.black,
-          ),
-          onPressed: () async {
-            // print('FloatingActionButton');
-            UserData result = await Navigator.of(context)
-                .pushNamed(Routers.createUserData) as UserData;
-
-            /// fake result
-            if (result != null) {
-              // result.appname = "cccc";
-              // result.userId = "asdasd";
-              _mainBloc.add(DataAdded(userData: result));
-            }
-          },
-          backgroundColor: Colors.yellow,
-        ),
-      );
-    });
-  }
-}
+part 'package:password_store_app/main/view/main_list_page.dart';
+part 'package:password_store_app/main/view/main_settings_page.dart';
 
 class MainPage extends StatelessWidget {
   @override
