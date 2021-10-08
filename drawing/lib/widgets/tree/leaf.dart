ace675c2191a2277bc5debfdc25cebdc28ae0865
guchengxi1994 <guchengxi1994@users.noreply.github.com>
2021-10-08 19:40:17 +0800

    Update leaf.dart

--- drawing/lib/widgets/tree/leaf.dart
+++ drawing/lib/widgets/tree/leaf.dart
@@ -1,1 +1,15 @@
-class TreeLeaf {}
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: xiaoshuyui
+ * @email: guchengxi1994@qq.com
+ * @Date: 2021-10-08 19:16:24
+ * @LastEditors: xiaoshuyui
+ * @LastEditTime: 2021-10-08 19:40:10
+ */
+import 'package:flutter/material.dart';
+
+class TreeLeaf {
+  Color? leafColor;
+  bool? shadow;
+}
